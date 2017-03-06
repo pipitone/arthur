4e876ed2aec7e0b72c553477d6b887f3aaf2d4be
Jon Pipitone <jon@pipitone.ca>
2017-03-05 17:30:58 -0800

    Autocomplete history questions

--- js/arthur.js
+++ js/arthur.js
@@ -1,50 +1,168 @@
-// set up stem and title
-$("#title").text(Case.title);
-$("p.stem").text(Case.stem);
+HxQs = [ 
+    "Birthplace",
+    "Occupation",
+    "Nationality",
+    "Education",
+    "Religion",
+    "Marital Status",
+    "Children",
+    "Pets",
+    "Drug Use",
+    "Tobacco Use",
+    "Alcohol Use",
+    "Exercise",
+    "Drinks",
+    "Travel",
+    "Caffeine Use",
+    "Cancer",
+    "Emphysema",
+    "Glaucoma",
+    "Anemia",
+    "Artery Disease",
+    "Arthritis",
+    "Asthma",
+    "Blood transfusions",
+    "Broken Bones",
+    "Chicken Pox",
+    "Cystic Fibrosis",
+    "Depression",
+    "Diabetes",
+    "Diseases",
+    "Herpes",
+    "HIV",
+    "Frequent Bladder Infection",
+    "Frequent Sinus Infections",
+    "Gallstones",
+    "Gonorrhea",
+    "Chlamydia",
+    "Gout",
+    "Head Injury",
+    "Heart Attack/Coronary",
+    "Heart failure",
+    "Heartburn / Reflux",
+    "Hepatitis B vaccine",
+    "Hepatitis / Jaundice",
+    "High BP",
+    "High Cholesterol",
+    "Hives",
+    "Infectious Mono",
+    "Immunizations",
+    "Intravenous drug abuse",
+    "Kidney Disease",
+    "Kidney Stones",
+    "Liver Disease",
+    "Malaria",
+    "Migraines",
+    "Needle injury",
+    "Osteoporosis",
+    "Pneumonia",
+    "Polio",
+    "Positive TB Skin Test",
+    "Prostate Enlargement",
+    "Rheumatic Fever",
+    "Seizures",
+    "STDs",
+    "Stroke",
+    "Syphilis",
+    "Tetanus booster",
+    "Thyroid Trouble",
+    "Tuberculosis",
+    "Ulcer disease",
+    ];
 
-// build the history table
-table = $("#hx");
-for (i in Case.history) { 
-    question = Case.history[i];
-    rowid ="hx-" + i;
-    table.append("<tr id='" + rowid + "'></tr>");
-    $("#"+rowid).append([ 
-            '<td class="importance-' + question["importance"] + '"><span class="icon"></span></td>',
-            '<td class="question"><span>' + question["question"] + '</div></td>', 
-            '<td class="answer"><span>' + question["answer"] + '</span></td>', 
-            '<td class="feedback">' + question["feedback"] + '</td>', 
+function populate_hx_row(row, question, response) {
+    row.append([ 
+            '<td class="importance-' + response["importance"] + '"><span class="icon"></span></td>',
+            '<td class="question"><span>' + question + '</div></td>', 
+            '<td class="response"><span>' + response["response"] + '</span></td>', 
+            '<td class="feedback">' + response["feedback"] + '</td>', 
 
-    ])
+    ]);
+    return row;
 }
 
-// show Hx feedback
-$("#hx-done").click(function() {
-    $("#hx tr .feedback").css("opacity", 1);
+$( document ).ready(function() {
+    // set up stem and title
+    $("#title").text(Case.title);
+    $("p.stem").text(Case.stem);
 
-    /** selected items that should have been asked  **/
-    $(".icon",  
-            $("#hx tbody tr.selected:not(:has(.importance-low))").addClass("success")
-     ).addClass("glyphicon glyphicon-ok");
+    // build the history table
+    var questions = Object.keys(Case.history).concat(HxQs);
 
-    /** selected items that shouldn't have been asked **/ 
-    $(".icon",  
-            $("#hx tbody tr.selected:has(.importance-low)").addClass("warning")
-     ).addClass("glyphicon glyphicon-exclamation-sign");
+    $( "#hx_autocomplete" ).autocomplete({
+        delay: 0, 
+        autoFocus: true,
+        source: questions,
+        select: function( event, ui ) {
+            question = event.target.value;
 
-    /** unselected items that should have been asked **/
-    $(".icon",  
-            $("#hx tbody tr:not(.selected):not(:has(.importance-low))").addClass("danger")
-     ).addClass("glyphicon glyphicon-remove");
+            // ignore partial questions
+            if (questions.indexOf(question) == -1) {
+                return;
+            }
 
-    /** unselected items that should not have been asked **/
-    $(".icon",  
-            $("#hx tbody tr:not(.selected):has(.importance-low)").addClass("success")
-     ).addClass("glyphicon glyphicon-ok");
-});
+            // skip this question if it has already been asked
+            if ($('#hx td.question:has(:contains("' + question + '"))').length != 0) {
+                return;
+            }
 
+            response = Case.history[question];
 
-$("#hx td.question").click(function (event) {
-    row = $(event.target).parentsUntil("table", "tr")
-        $(".answer", row).css("opacity", 1);
-    row.addClass("selected");
+            // No response in this case, so it likely isn't important
+            if (!response) {
+                response = {
+                    "importance" : "low", 
+                    "response" : "", 
+                    "feedback" : "None."
+                }
+            }
+
+            row = $(document.createElement("tr")).addClass('selected');
+            $("#hx").append(populate_hx_row(row, question, response)); 
+
+            $(".selected .response").css("opacity", 1);
+        }, 
+    });
+
+    // show Hx feedback
+    $("#hx-done").click(function() {
+        // show missing important history questions
+        table = $("#hx");
+        for (question in Case.history) { 
+            response = Case.history[question];
+
+            if ($('#hx td.question:has(:contains("' + question + '"))').length != 0 || 
+                    response.importance != "high") {
+                continue; 
+            }
+
+            row = $(document.createElement("tr"));
+            $("#hx").append(populate_hx_row(row, question, response)); 
+        }
+        
+        //
+        $("#hx tr .feedback").css("opacity", 1);
+
+        /** selected items that should have been asked  **/
+        $(".icon",  
+                $("#hx tbody tr.selected:not(:has(.importance-low))").addClass("success")
+         ).addClass("glyphicon glyphicon-ok success");
+
+        /** selected items that shouldn't have been asked **/ 
+        $(".icon",  
+                $("#hx tbody tr.selected:has(.importance-low)").addClass("warning")
+         ).addClass("glyphicon glyphicon-exclamation-sign alert-danger");
+
+        /** unselected items that should have been asked **/
+        $(".icon",  
+                $("#hx tbody tr:not(.selected):not(:has(.importance-low))").addClass("danger")
+         ).addClass("glyphicon glyphicon-remove");
+
+        /** unselected items that should not have been asked **/
+        $(".icon",  
+                $("#hx tbody tr:not(.selected):has(.importance-low)").addClass("success")
+         ).addClass("glyphicon glyphicon-ok");
+
+        $("td:has(.icon)").css("background-color", "white");
+    });
 });
