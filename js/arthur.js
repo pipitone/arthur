2f06970386275deaaafe64ce6c93c5dc34594767
anyhowes <nathan.how@live.ca>
2017-04-29 13:27:28 -0400

    Updating History and Physical sections

--- js/arthur.js
+++ js/arthur.js
@@ -1,16 +1,14 @@
-HxQs = [ "Birthplace", "Occupation", "Nationality", "Education", "Religion", "Marital Status", "Children", "Pets", "Drug Use", "Tobacco Use", "Alcohol Use", "Exercise", "Drinks", "Travel", "Caffeine Use", "Cancer", "Emphysema", "Glaucoma", "Anemia", "Artery Disease", "Arthritis", "Asthma", "Blood transfusions", "Broken Bones", "Chicken Pox", "Cystic Fibrosis", "Depression", "Diabetes", "Diseases", "Herpes", "HIV", "Frequent Bladder Infection", "Frequent Sinus Infections", "Gallstones", "Gonorrhea", "Chlamydia", "Gout", "Head Injury", "Heart Attack/Coronary", "Heart failure", "Heartburn / Reflux", "Hepatitis B vaccine", "Hepatitis / Jaundice", "High BP", "High Cholesterol", "Hives", "Infectious Mono", "Immunizations", "Intravenous drug abuse", "Kidney Disease", "Kidney Stones", "Liver Disease", "Malaria", "Migraines", "Needle injury", "Osteoporosis", "Pneumonia", "Polio", "Positive TB Skin Test", "Prostate Enlargement", "Rheumatic Fever", "Seizures", "STDs", "Stroke", "Syphilis", "Tetanus booster", "Thyroid Trouble", "Tuberculosis", "Ulcer disease", ];
-
 Diagnoses = [ "AIDS", "allergies", "Alzheimer's disease", "anxiety disorders", "arthritis", "asthma", "astigmatism", "autoimmune diseases", "benign prostate hyperplasia (BPH)", "bipolar disorder (manic-depressive)", "brain cancer", "breast cancer", "cancer", "candidiasis", "cataracts", "celiac disease", "cervical cancer", "chicken pox", "chlamydia", "chronic fatigue syndrome (CFS)", "chronic illness", "cold sores", "colon cancer", "constipation", "common cold", "COPD", "cough", "Crohn's disease", "cystic fibrosis", "dementia", "diabetes", "diarrhea", "depression", "eczema", "endometriosis", "eye disorders", "fibroids", "fibromyalgia", "flu (influenza)", "food poisoning", "Gallstones", "genital herpes", "gonorrhea", "Graves' disease", "Hashimoto's thyroiditis", "hay fever", "headache", "heart disease", "hemochromatosis", "hepatitis", "herpes", "high cholesterol", "HIV", "Hodgkin's disease", "HPV (human papilloma virus)", "hypertension", "impotence", "insomnia", "irritable bowel syndrome", "jaundice", "kidney disease", "lactose intolerance", "leukemia", "liver cancer", "liver disease", "lung cancer", "lupus", "Lyme disease", "lymphoma", "meningitis", "meningococcal disease", "menopause", "mental illness", "myopia (short-sightedness)", "migraine", "multiple sclerosis", "muscular dystrophy", "narcolepsy", "Non-Hodgkin's lymphoma", "obesity", "osteoporosis", "otitis media (middle ear infection)", "ovarian cancer", "overweight", "pain", "Parkinson's disease", "pelvic inflammatory disease", "pertussis", "pregnancy", "premenstrual syndrome (PMS)", "prostate cancer", "prostate disorders", "Raynaud's Phenomenon", "SARS", "sexually transmitted diseases", "sleep disorders", "smoking", "stroke", "thrush", "thyroid disorders", "whooping cough ", ];
 
 var Case;
+var Easy = true;
 
-function populate_hx_row(row, question, response) {
+function populate_hx_row(row, question) {
     row.append([
-            '<td class="importance-' + response["importance"] + '"><span class="icon"></span></td>',
-            '<td class="question"><span>' + question + '</span></td>',
-            '<td class="response"><span>' + response["response"] + '</span></td>',
-            '<td class="feedback">' + response["feedback"] + '</td>',
-
+            '<td class="importance-' + question["importance"] + '"><span class="icon"></span></td>',
+            '<td class="question"><span>' + question["query"] + '</span></td>',
+            '<td><span class="response" style="display:none">' + question["response"] + '</span></td>',
+            '<td><span class="feedback" style="display:none">' + question["feedback"] + '</span></td>',
     ]);
     return row;
 }
@@ -29,8 +27,16 @@ $( document ).ready(function() {
      */
 
     // build the history table
-    var questions = Object.keys(Case.history).concat(HxQs);
-
+    var questions = Case.history;
+    for (i=0;i<questions.length;i++){
+      row = $(document.createElement("tr")).addClass('hx-row');
+      $("#hx").append(populate_hx_row(row, questions[i]));
+    }
+    var exams = Case.physical;
+    for (i=0;i<exams.length;i++){
+      row = $(document.createElement("tr")).addClass('px-row');
+      $("#physical").append(populate_hx_row(row, exams[i]));
+    }
     $( "#hx_autocomplete" ).autocomplete({
         delay: 0,
         autoFocus: true,
@@ -59,20 +65,22 @@ $( document ).ready(function() {
                 }
             }
 
-            row = $(document.createElement("tr")).addClass('selected');
-            $("#hx").append(populate_hx_row(row, question, response));
-
-            $(".selected .response").css("opacity", 1);
+            row = $(document.createElement("tr")).addClass('selected', 'hx-row');
+            $("#hx").append(populate_hx_row(row, response));
 
             this.value = "";
             return false;
         },
     });
-
+    // show Hx result
+    $(".hx-row .px-row").click(function() {
+      $(this).addClass('active selected');
+      $(this).find('.response').show();
+    })
     // show Hx feedback
     $("#hx-done").click(function() {
         // show missing important history questions
-        table = $("#hx");
+        /*table = $("#hx");
         for (question in Case.history) {
             response = Case.history[question];
 
@@ -83,10 +91,9 @@ $( document ).ready(function() {
 
             row = $(document.createElement("tr"));
             $("#hx").append(populate_hx_row(row, question, response));
-        }
-
-        //
-        $("#hx tr .feedback").css("opacity", 1);
+        }*/
+        $('#hx .response').show();
+        $('#hx .feedback').show();
 
         /** selected items that should have been asked  **/
         $(".icon",
--- js/case-elong.js
+++ js/case-elong.js
@@ -1,46 +1,41 @@
+//History Overrides
+mood.response = "I've been feeling a little low";
+mood.feedback = "Important to explore possible mood disorder";
+mood.importance = "high";
+diet.response = "I miss meals frequently";
+diet.feedback = "Suspect nutritional (iron, B12/folate) deficiency";
+diet.importance = "high";
+sleep.response = "I've been so sleepy and sleeping all the time.";
+sleep.feedback = "Important. Suspect sleep disorders.";
+sleep.importance = "high";
+medications.response = "Diazepam 5mg for anxiety NKDA";
+medications.feedback = "Important to ask as symptoms may be a meds adverse effect.";
+medications.importance = "high";
+bloodTransfusions.feedback = "Important if you suspect some form of anemia";
+bloodTransfusions.importance = "med";
+//Physical Overrides
+generalInspection.response = "You see a thin, pale woman who appears her stated age. She is evidently anxious.";
+generalInspection.feedback = "Initial impression of the health of the pagient";
+vitals.response = "HR: 90<br> BP: 120/80 supine, no postural change<br> Weight: 52 kg<br> Height: 173cm";
+vitals.feedback = "Vitals are vital";
+vitals.importance = "high";
+cardiovascular.response = "Her mucous membranes are pale. She has a grade 2/6 apical systolic murmur. There are no other significant findings";
+cardiovascular.feedback = "Possible cardiac cause for her fatigue. May show signs of anemia.";
+cardiovascular.importance = "high";
+respiratory.response = "Air entry is normal. There are occasional scattered wheezes throughout both lung fields.";
+respiratory.feedback = "Relevant given her smoking history.";
+respiratory.importance = "high";
+pelvic.response = "Vaginal exam is normal. Bimanual exam reveals a slightly enlarged, non-tender uterus.";
+pelvic.feedback = "Look for abnormalities that may indicate the source of the bleeding.";
+pelvic.importance = "high";
+dre.response = "Negative for occult blood.";
+dre.feedback = "Alternate sources of bleeding, presence of anovaginal fistula";
+dre.importance = "med";
 ELong = {
     "title" : "Elizabeth Long",
     "stem" : "Elizabeth Long, a forty-five-year-old divorced mother of three teenaged children, comes to you, her family physician, with a six-month history of fatigue. She tells you that she feels tired all the time and that she doesn’t even have the energy to climb the stairs at her apartment.",
-    "history" : {
-        "Mood" : {
-            "response": "I've been feeling a little low.",
-            "feedback": "Important to explore possible mood disorder, and impact of illness.",
-            "importance": "high"
-        },
-        "Diet" : {
-            "response": "I miss meals frequently.",
-            "feedback": "Important. Suspect nutritional (iron, B12/folate) deficiency.",
-            "importance": "high"
-        },
-        "Sleep" : {
-            "response": "I've been so sleepy and sleeping all the time.",
-            "feedback": "Important. Suspect sleep disorders.",
-            "importance": "high"
-        },
-        "Joint Pain" : {
-            "response": "None",
-            "feedback": "Not very important in this case unless suggested elsewhere.",
-            "importance": "low"
-        },
-        "Medications" : {
-            "response" : "Diazepam 5mg for anxiety NKDA",
-            "feedback": "Important to ask as symptoms may be a meds adverse effect.",
-            "importance": "high"
-        },
-        "ASOUL" : {
-            "response": "45 year old divorced mother of three teenaged children living in a house in north Kingston",
-            "feedback": "Always useful to ask to understand context, and often reveals clues about how illness manifests, and should be managed.",
-            "importance": "high"
-        },
-        "Substance Use" : {
-            "response": "None",
-            "feedback": "Important.",
-            "importance": "high"
-        },
-    },
-    "physical" : {
-
-    },
+    "history" : [mood,diet,sleep,jointPain,medications,maritalStatus,children,drugUse,bloodTransfusions],
+    "physical" : [generalInspection,vitals,cardiovascular,respiratory,pelvic,dre],
     "investigations" : {
         "CBC": {
             result: "",
